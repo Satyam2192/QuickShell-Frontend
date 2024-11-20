b005ff3f64439d07c5f3d53475fa6e4026fff626
Satyam <satyam21092@gmail.com>
2024-11-20 08:34:10 +0530

    First Commit

--- src/components/UserIcon/index.tsx
+++ src/components/UserIcon/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import './usericon.css'
+import './usericon.css';
 
 function UserIcon({ name, available }: { name: string, available: boolean }) {
+    const colors = ["#2b963a", "#3498db", "#9b59b6", "#e74c3c", "#f1c40f", "#1abc9c", "#e67e22"];
+    const randomColor = React.useMemo(() => {
+        return colors[Math.floor(Math.random() * colors.length)];
+    }, []); 
+
     const text = React.useMemo(() => {
         return name.split(" ").map((item: string) => item[0]).join("");
     }, [name]);
 
     return (
-        <div className='usericon-container'>
+        <div className='usericon-container' style={{ backgroundColor: randomColor }}>
             <div className='usericon-text'>{text}</div>
             <div className={`user-status ${available && "available"}`}></div>
         </div>
